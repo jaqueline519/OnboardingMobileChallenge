115a3cdf733a9986de6551cc0a7113f38730a25f
Jonattan <jonattan.sousa@gmail.com>
2021-04-27 20:23:54 -0300

    Passado testes de verificacao de linhas da tableView da Home para arquivo de testes dedicados a classe, melhorando a organizacao.

--- OnboardingMobileChallengeTests/HomeTableTableViewControllerTests.swift
+++ OnboardingMobileChallengeTests/HomeTableTableViewControllerTests.swift
@@ -6,11 +6,28 @@
 //
 
 import XCTest
+import ModuloCommons
+import ModuloDetalhes
+@testable import OnboardingMobileChallenge
 
 class HomeTableTableViewControllerTests: XCTestCase {
+    var homeController: HomeTableTableViewController!
+    let verificador = 5
+    var dado: ListaMoeda! = nil
 
-    override class func setUp() {
-        <#code#>
+    override func setUp() {
+        homeController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeTable") as? HomeTableTableViewController
+        _ = homeController.view
+        
+        dado = ListaMoeda(assetID: "BTC", name: "Bitcoin", typeIsCrypto: 1, dataStart: "2021", dataEnd: "2022", dataQuoteStart: "2010", dataQuoteEnd: "2011", dataOrderbookStart: "1995", dataOrderbookEnd: "1890", dataTradeStart: "2001", dataTradeEnd: "2005", dataSymbolsCount: 2, volume1HrsUsd: 20.0, volume1DayUsd: 50.5, volume1MthUsd: 30.2, priceUsd: 19900.0, idIcon: "sucesso")
+        
+        for _ in 1...verificador {
+            homeController.dadosListaMoedas.append(dado)
+        }
+    }
+    
+    func testSeNumeroDeLinhasDaTabelaIgualACinco() {
+        XCTAssertEqual(homeController.tableView.numberOfRows(inSection: 0), 5, "Numero de linhas da tabela sendo igual ao valor atribuido em varificador, cinco")
     }
     
 
--- OnboardingMobileChallengeTests/OnboardingMobileChallengeTests.swift
+++ OnboardingMobileChallengeTests/OnboardingMobileChallengeTests.swift
@@ -12,24 +12,14 @@ import ModuloDetalhes
 
 class OnboardingMobileChallengeTests: XCTestCase {
 
-    var homeController: HomeTableTableViewController!
     var detalhesVC: DetalhesViewController!
     var homeTvCell: HomeUITableViewCell!
     var dado: ListaMoeda! = nil
-    let verificador = 5
     
     override func setUpWithError() throws {
-        homeController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeTable") as? HomeTableTableViewController
-        _ = homeController.view
         dado = ListaMoeda(assetID: "BTC", name: "Bitcoin", typeIsCrypto: 1, dataStart: "2021", dataEnd: "2022", dataQuoteStart: "2010", dataQuoteEnd: "2011", dataOrderbookStart: "1995", dataOrderbookEnd: "1890", dataTradeStart: "2001", dataTradeEnd: "2005", dataSymbolsCount: 2, volume1HrsUsd: 20.0, volume1DayUsd: 50.5, volume1MthUsd: 30.2, priceUsd: 19900.0, idIcon: "sucesso")
         detalhesVC = DetalhesViewController()
         homeTvCell = HomeUITableViewCell()
-        
-//        let dados: MoedaInfoElement?
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        for _ in 1...verificador {
-            homeController.dadosListaMoedas.append(dado)
-        }
     }
 
     override func tearDownWithError() throws {
@@ -48,11 +38,7 @@ class OnboardingMobileChallengeTests: XCTestCase {
         _ = detalhesVC.consultaBD()
         detalhesVC.addFavorito("ETH")
         detalhesVC.removeFavorito("ETH")
-//        DetalhesViewController().removeTodosOsFavoritos()  // Não queremos isso
         _ = detalhesVC.resgateFavoritos()
         detalhesVC.atualizaBD()
-    }
-    func testHomeTableView() {
-        XCTAssertEqual(homeController.tableView.numberOfRows(inSection: 0), verificador)
     }
 }
